3077ba18c17d9327f6fbb4ede9ef213f0f606ad7
Chinedu Samuel Asiabaka <110354688+samuelasiabaka@users.noreply.github.com>
2023-02-07 19:46:45 +0100

    Renamed title

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCT-_Puy0UBJbgdZ5mW8-Uh1BDd1oko0Zc",
-  authDomain: "chat-19025.firebaseapp.com",
-  projectId: "chat-19025",
-  storageBucket: "chat-19025.appspot.com",
-  messagingSenderId: "207862793348",
-  appId: "1:207862793348:web:e24bad884fc4193de56ed1"
-};
+  apiKey: 'AIzaSyCT-_Puy0UBJbgdZ5mW8-Uh1BDd1oko0Zc',
+  authDomain: 'chat-19025.firebaseapp.com',
+  projectId: 'chat-19025',
+  storageBucket: 'chat-19025.appspot.com',
+  messagingSenderId: '207862793348',
+  appId: '1:207862793348:web:e24bad884fc4193de56ed1',
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const storage = getStorage();
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
+export const storage = getStorage()
 export const db = getFirestore()
